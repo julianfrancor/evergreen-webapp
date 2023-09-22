ff0ad842a5fab6da59f6373215149afe9e2d41b8
Julian <julianfrancor@gmail.com>
2023-09-22 10:44:39 -0500

    Update page with form

--- app/page2.tsx
+++ app/page2.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 
 export default function Home() {
-
   const [formData, setFormData] = useState({
-    username: '',
-    name: '',
-    last_name: '',
-    user_type: '',
-    password: '',
-    status: ''
+    fName: '',
+    lName: '',
+    guest: '',
+    date: '',
+    time: '',
+    radioButton: '',
   });
 
   const [response, setResponse] = useState(null);
@@ -24,6 +23,7 @@ export default function Home() {
   const handleSubmit = (e:any) => {
     e.preventDefault();
     callAPI('https://evergreen-adm.azurewebsites.net/users', 'POST', formData);
+    console.log(formData);
   };
 
   const handleGetAllUsers = () => {
@@ -51,84 +51,176 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input 
-            type="text" 
-            name="username" 
-            value={formData.username} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Name:</label>
-          <input 
-            type="text" 
-            name="name" 
-            value={formData.name} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Last Name:</label>
-          <input 
-            type="text" 
-            name="last_name" 
-            value={formData.last_name} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <div>
-          <label>User Type:</label>
-          <input 
-            type="text" 
-            name="user_type" 
-            value={formData.user_type} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input 
-            type="password" 
-            name="password" 
-            value={formData.password} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Status:</label>
-          <input 
-            type="text" 
-            name="status" 
-            value={formData.status} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <button type="submit">Create User</button>
-      </form>
+    <div className="flex items-center justify-center p-12">
+      <div className="mx-auto w-full max-w-[550px]">
+        <form onSubmit={handleSubmit}>
+          <div className="-mx-3 flex flex-wrap">
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
+                <label
+                  htmlFor="fName"
+                  className="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="fName"
+                  id="fName"
+                  placeholder="First Name"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  value={formData.fName}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
+                <label
+                  htmlFor="lName"
+                  className="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="lName"
+                  id="lName"
+                  placeholder="Last Name"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  value={formData.lName}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
+                <label
+                  htmlFor="fName"
+                  className="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="fName"
+                  id="fName"
+                  placeholder="First Name"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  value={formData.fName}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="guest"
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              How many guests are you bringing?
+            </label>
+            <input
+              type="number"
+              name="guest"
+              id="guest"
+              placeholder="5"
+              min="0"
+              className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              value={formData.guest}
+              onChange={handleChange}
+            />
+          </div>
 
-      <div>
-        <button onClick={handleGetAllUsers}>Get all Users</button>
-        <button onClick={handleGetUserById}>Get User by Id</button>
-      </div>
+          <div className="-mx-3 flex flex-wrap">
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
+                <label
+                  htmlFor="date"
+                  className="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                  Date
+                </label>
+                <input
+                  type="date"
+                  name="date"
+                  id="date"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  value={formData.date}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
+                <label
+                  htmlFor="time"
+                  className="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                  Time
+                </label>
+                <input
+                  type="time"
+                  name="time"
+                  id="time"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  value={formData.time}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+          </div>
 
-      <div>
-        {response && (
-          <pre>
-            {JSON.stringify(response, null, 2)}
-          </pre>
-        )}
+          <div className="mb-5">
+            <label className="mb-3 block text-base font-medium text-[#07074D]">
+              Are you coming to the event?
+            </label>
+            <div className="flex items-center space-x-6">
+              <div className="flex items-center">
+                <input
+                  type="radio"
+                  name="radioButton"
+                  id="radioButton1"
+                  className="h-5 w-5"
+                  value="Yes"
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="radioButton1"
+                  className="pl-3 text-base font-medium text-[#07074D]"
+                >
+                  Yes
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  type="radio"
+                  name="radioButton"
+                  id="radioButton2"
+                  className="h-5 w-5"
+                  value="No"
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="radioButton2"
+                  className="pl-3 text-base font-medium text-[#07074D]"
+                >
+                  No
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
-    
   );
 }
