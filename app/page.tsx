47e8fed65bf4dfd19c82c10d8addf9b8984ab44d
Julian <julianfrancor@gmail.com>
2023-09-22 19:14:31 -0500

    Add form to create user

--- app/page.tsx
+++ app/page.tsx
@@ -1,135 +1,231 @@
-'use client'
-import React, { useState } from 'react';
+import { NextResponse } from 'next/server'
 
-export default function Home() {
 
-  const [formData, setFormData] = useState({
-    username: '',
-    name: '',
-    last_name: '',
-    user_type: '',
-    password: '',
-    status: ''
-  });
+interface FormData {
+  name: string;
+  value: string;
+}
 
-  const [response, setResponse] = useState(null);
+function convertData(dataForm: FormData[]): Record<string, string> {
+  const result: Record<string, string> = {};
 
-  const handleChange = (e:any) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e:any) => {
-    e.preventDefault();
-    callAPI('https://evergreen-adm.azurewebsites.net/users', 'POST', formData);
-  };
-
-  const handleGetAllUsers = () => {
-    callAPI('https://evergreen-adm.azurewebsites.net/users', 'GET', null);
-  };
-
-  const handleGetUserById = () => {
-    callAPI('https://evergreen-adm.azurewebsites.net/users/1', 'GET', null);
-  };
-
-  const callAPI = async (url:any, method:any, body:any) => {
-    try {
-      const res = await fetch(url, {
-        method: method,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body)
-      });
-      const data = await res.json();
-      setResponse(data);
-    } catch (err) {
-      console.error(err);
+  for (const item of dataForm) {
+    switch (item.name) {
+      case 'username':
+        result['username'] = item.value;
+        break;
+      case 'firstName':
+        result['name'] = item.value;
+        break;
+      case 'lastName':
+          result['last_name'] = item.value;
+          break;
+      case 'userType':
+        result['user_type'] = item.value;
+        break;
+      case 'password':
+          result['password'] = item.value;
+          break;
+      case 'status':
+        result['status'] = item.value;
+        break;
+      default:
+        break;
     }
-  };
+  }
+
+  return result;
+}
+
+
+
+
+export async function createUser(convertedData) {
+  const res = await fetch('https://evergreen-adm.azurewebsites.net/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': process.env.DATA_API_KEY,
+    },
+    body: JSON.stringify(convertedData),
+  })
+ 
+  const data = await res.json()
+  console.log(data)
+ 
+  return NextResponse.json(data)
+}
+
+
+
+async function getData() {
+  const res = await fetch('https://evergreen-adm.azurewebsites.net/users')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+ 
+
+export default async function Page() {
+  const data = await getData()
+  console.log(data)
+  
+  async function create(formData: FormData) {
+    'use server'
+    // mutate data
+    // revalidate cache
+    console.log(formData)
+    const convertedData = convertData(formData);
+    console.log(convertedData);
+    createUser(convertedData)
+  }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input 
-            type="text" 
-            name="username" 
-            value={formData.username} 
-            onChange={handleChange} 
-            required 
-          />
+  <main>
+    <div className="flex items-center justify-center p-12">
+      <div className="mx-auto w-full max-w-[550px]">
+      
+      <form action={create}>
+        <div className="-mx-3 flex flex-wrap">
+          {/* FIRST NAME */}
+          <div className="w-full px-3 sm:w-1/2">
+            <div className="mb-5">
+              <label
+                htmlFor="firstName"
+                className="mb-3 block text-base font-medium text-[#07074D]"
+              >
+                First Name
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                placeholder="First Name"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              />
+            </div>
+          </div>
+
+          {/* LAST NAME */}
+          <div className="w-full px-3 sm:w-1/2">
+            <div className="mb-5">
+              <label
+                htmlFor="lastName"
+                className="mb-3 block text-base font-medium text-[#07074D]"
+              >
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                placeholder="Last Name"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              />
+            </div>
+          </div>
+
+          {/* USERNAME */}
+          <div className="w-full px-3 sm:w-1/2">
+            <div className="mb-5">
+              <label
+                htmlFor="username"
+                className="mb-3 block text-base font-medium text-[#07074D]"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Username"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              />
+            </div>
+          </div>
+
+          {/* USER TYPE */}
+          <div className="w-full px-3 sm:w-1/2">
+            <div className="mb-5">
+              <label
+                htmlFor="userType"
+                className="mb-3 block text-base font-medium text-[#07074D]"
+              >
+                User Type
+              </label>
+              <select
+                name="userType"
+                id="userType"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              >
+                <option value="admin">Admin</option>
+                <option value="user">User</option>
+              </select>
+            </div>
+          </div>
+
+          {/* PASSWORD */}
+          <div className="w-full px-3 sm:w-1/2">
+            <div className="mb-5">
+              <label
+                htmlFor="password"
+                className="mb-3 block text-base font-medium text-[#07074D]"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              />
+            </div>
+          </div>
+
+          {/* STATUS */}
+          <div className="w-full px-3 sm:w-1/2">
+            <div className="mb-5">
+              <label
+                htmlFor="status"
+                className="mb-3 block text-base font-medium text-[#07074D]"
+              >
+                Status
+              </label>
+              <select
+                name="status"
+                id="status"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              >
+                <option value="active">Active</option>
+                <option value="inactive">Inactive</option>
+              </select>
+            </div>
+          </div>
         </div>
+
         <div>
-          <label>Name:</label>
-          <input 
-            type="text" 
-            name="name" 
-            value={formData.name} 
-            onChange={handleChange} 
-            required 
-          />
+          <button
+            type="submit"
+            className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
+          >
+            Submit
+          </button>
         </div>
-        <div>
-          <label>Last Name:</label>
-          <input 
-            type="text" 
-            name="last_name" 
-            value={formData.last_name} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <div>
-          <label>User Type:</label>
-          <input 
-            type="text" 
-            name="user_type" 
-            value={formData.user_type} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input 
-            type="password" 
-            name="password" 
-            value={formData.password} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Status:</label>
-          <input 
-            type="text" 
-            name="status" 
-            value={formData.status} 
-            onChange={handleChange} 
-            required 
-          />
-        </div>
-        <button type="submit">Create User</button>
       </form>
 
-      <div>
-        <button onClick={handleGetAllUsers}>Get all Users</button>
-        <button onClick={handleGetUserById}>Get User by Id</button>
-      </div>
-
-      <div>
-        {response && (
-          <pre>
-            {JSON.stringify(response, null, 2)}
-          </pre>
-        )}
+      
       </div>
     </div>
-    
-  );
+  </main>)
 }
+
+
+
+ 
