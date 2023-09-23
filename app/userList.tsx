a09c2c4e1ce755abdd36ffec6cf3c21996f0f226
Julian <julianfrancor@gmail.com>
2023-09-23 00:51:02 -0500

    improve styles

--- app/userList.tsx
+++ app/userList.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React from 'react';
+import './UserList.css';
 
 interface User {
   last_name: string;
@@ -18,19 +19,34 @@ interface UserListProps {
 
 function UserList({ users }: UserListProps) {
   return (
-    <div>
-      <h2>User List</h2>
-      <ul>
-        {users.map((user, index) => (
-          <li key={user.id}>
-            <div>Username: {user.username}</div>
-            <div>Name: {user.name}</div>
-            <div>Last Name: {user.last_name}</div>
-            <div>User Type: {user.user_type}</div>
-            <div>Status: {user.status}</div>
-          </li>
-        ))}
-      </ul>
+    <div className="user-card-grid">
+      {users.map((user, index) => (
+        <div className="user-card" key={user.id}>
+          <img className="user-avatar" src={user.photo} alt={`Avatar for ${user.name}`} />
+          <div className="user-info">
+            <div className="user-info-row">
+              <div className="label">Username:</div>
+              <div className="value">{user.username}</div>
+            </div>
+            <div className="user-info-row">
+              <div className="label">Name:</div>
+              <div className="value">{user.name}</div>
+            </div>
+            <div className="user-info-row">
+              <div className="label">Last Name:</div>
+              <div className="value">{user.last_name}</div>
+            </div>
+            <div className="user-info-row">
+              <div className="label">User Type:</div>
+              <div className="value">{user.user_type}</div>
+            </div>
+            <div className="user-info-row">
+              <div className="label">Status:</div>
+              <div className="value">{user.status}</div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
